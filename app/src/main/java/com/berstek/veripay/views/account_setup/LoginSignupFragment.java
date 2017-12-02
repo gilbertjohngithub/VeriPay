0079e4e83b606530ebbc3aa8a6c3b59a70492c44
quasarjohn <gilbertjohngithub@gmail.com>
2017-12-02 13:58:06 +0800

    Animation on login page //TODO change color scheme

--- app/src/main/java/com/berstek/veripay/views/account_setup/LoginSignupFragment.java
+++ app/src/main/java/com/berstek/veripay/views/account_setup/LoginSignupFragment.java
@@ -1,11 +1,13 @@
 package com.berstek.veripay.views.account_setup;
 
 
+import android.animation.Animator;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,7 +18,7 @@ import com.berstek.veripay.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LoginSignupFragment extends Fragment {
+public class LoginSignupFragment extends Fragment implements View.OnClickListener{
 
     private View view;
     private Button signupBtn, loginBtn;
@@ -51,7 +53,102 @@ public class LoginSignupFragment extends Fragment {
         googleLoginBtn = view.findViewById(R.id.login_google);
         logo = view.findViewById(R.id.logo);
 
+        signupBtn.setOnClickListener(this);
+        loginBtn.setOnClickListener(this);
+        googleLoginBtn.setOnClickListener(this);
+
 
         return view;
+    }
+
+    private void rippleLogin() {
+        signupForm.setVisibility(View.VISIBLE);
+        loginForm.setVisibility(View.VISIBLE);
+        loginForm.bringToFront();
+
+        int cx = 0 + (signupBtn.getWidth() / 2);
+        int cy = view.getHeight() - ((signupBtn.getHeight() / 2) + 16);
+        int rad = Math.max(view.getWidth(), view.getHeight());
+
+        Animator animator = ViewAnimationUtils.createCircularReveal(loginForm, cx, cy, 0, rad);
+        animator.setDuration(300);
+        animator.start();
+
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                signupForm.setVisibility(View.GONE);
+                getActivity().getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimary));
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+    }
+
+    private void rippleSignUp() {
+        signupForm.setVisibility(View.INVISIBLE);
+        signupForm.setVisibility(View.VISIBLE);
+        signupForm.bringToFront();
+
+        int cx = view.getWidth() - (loginBtn.getWidth() / 2);
+        int cy = view.getHeight() - ((loginBtn.getHeight() / 2) + 16);
+        int rad = Math.max(view.getWidth(), view.getHeight());
+
+        Animator animator = ViewAnimationUtils.createCircularReveal(signupForm, cx, cy, 0, rad);
+        animator.setDuration(300);
+        animator.start();
+
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                loginForm.setVisibility(View.GONE);
+                getActivity().getWindow().setStatusBarColor(getResources().getColor(R.color.colorAccentDark));
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.signup_btn:
+                rippleSignUp();
+                break;
+            case R.id.login_btn:
+                rippleLogin();
+                break;
+            case R.id.login_google:
+                //TODO google login
+                break;
+            default:
+                break;
+        }
     }
 }
