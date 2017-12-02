e6834afcbff2b26fdd16f6a7f87e26ede0949fcc
quasarjohn <gilbertjohngithub@gmail.com>
2017-12-02 18:27:26 +0800

    Profile activity updates to home fragment

--- app/src/main/java/com/berstek/veripay/views/home/HomeActivity.java
+++ app/src/main/java/com/berstek/veripay/views/home/HomeActivity.java
@@ -1,8 +1,8 @@
 package com.berstek.veripay.views.home;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.constraint.ConstraintLayout;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,12 +10,40 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.berstek.veripay.MainActivity;
 import com.berstek.veripay.R;
 
+import com.berstek.veripay.data_access.UserDA;
+import com.berstek.veripay.models.Contact;
+import com.berstek.veripay.models.User;
+import com.berstek.veripay.utils.CustomImageUtils;
+import com.berstek.veripay.utils.UserUtils;
+import com.berstek.veripay.views.account_setup.EditUserInfoActivity;
+import com.berstek.veripay.views.search.SearchContactsAdapter;
+import com.berstek.veripay.views.search.SearchResultsAdapter;
+import com.berstek.veripay.views.search.SearchUserDialogFragment;
+import com.berstek.veripay.views.user_profile.ProfileActivity;
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 public class HomeActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener,
+        SearchContactsAdapter.OnContactSelectedListener,
+        SearchResultsAdapter.OnResultSelectedListener {
+
+    private ImageView searchBtn;
+    private TextView appTitle, nameTxt, addressTxt, phoneTxt,
+            payIDTxt, ratingTxt, verifiedTxt;
+    private ImageView dpBlurred, dp;
+    private ConstraintLayout navHeader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,28 +52,70 @@ public class HomeActivity extends AppCompatActivity
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab =  findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //Wire views
+
+        navHeader = (ConstraintLayout) navigationView.getHeaderView(0);
+
+        searchBtn = findViewById(R.id.search_btn);
+        appTitle = findViewById(R.id.app_title);
+        dpBlurred = navHeader.findViewById(R.id.dp_blurred);
+        dp = navHeader.findViewById(R.id.dp);
+        nameTxt = navHeader.findViewById(R.id.name_txt);
+        addressTxt = navHeader.findViewById(R.id.address_edit);
+        phoneTxt = navHeader.findViewById(R.id.phone_txt);
+        payIDTxt = navHeader.findViewById(R.id.pay_id);
+        ratingTxt = navHeader.findViewById(R.id.rating_txt);
+        verifiedTxt = navHeader.findViewById(R.id.verified_txt);
+
+
+        searchBtn.setOnClickListener(this);
+
+        getSupportFragmentManager().beginTransaction().
+                add(R.id.home_content, new HomeViewPagerFragment()).commit();
+
+        loadUserInfo();
+    }
+
+
+    private void loadUserInfo() {
+        UserDA userDA = new UserDA();
+
+        Query query = userDA.queryUserByUID(UserUtils.getUID());
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    final User user = child.getValue(User.class);
+                    nameTxt.setText(user.getFullName());
+                    payIDTxt.setText("PAY ID: " + user.getPay_id());
+                    addressTxt.setText(user.getAddress_city() + ", Philippines");
+                    Glide.with(HomeActivity.this).
+                            load(user.getPhoto_url()).skipMemoryCache(true).into(dp);
+
+                    new CustomImageUtils().blurImage(HomeActivity.this,
+                            user.getPhoto_url(), dpBlurred, false);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -54,49 +124,51 @@ public class HomeActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_logout) {
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+        } else if (id == R.id.nav_settings) {
+            Intent intent = new Intent(this, EditUserInfoActivity.class);
+//            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        int id = view.getId();
+
+        if (id == R.id.search_btn) {
+            SearchUserDialogFragment fragment = new SearchUserDialogFragment();
+            fragment.setContactSelectedListener(this);
+            fragment.setResultSelectedListener(this);
+            fragment.show(getSupportFragmentManager(), null);
+        }
+    }
+
+    @Override
+    public void onContactSelected(Contact contact) {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        intent.putExtra("key", contact.getKey());
+        startActivity(intent);
+    }
+
+    @Override
+    public void onResultSelected(User user) {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        intent.putExtra("key", user.getKey());
+        startActivity(intent);
     }
 }
